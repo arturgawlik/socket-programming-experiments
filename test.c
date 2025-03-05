b59597df4e4ba701d932d8bb00bc33f4adffe199
arturgawlik <artur.gawlik@icloud.com>
2025-03-06 00:15:05 +0100

    connect, listen, accept

--- test.c
+++ test.c
@@ -88,6 +88,35 @@ int make_dns_lookup() {
     return result;
 }
 
+int get_socket(const char* address, struct addrinfo** results, char* port) {
+    struct addrinfo hints;
+
+    memset(&hints, 0, sizeof hints);
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (address == NULL) {
+        hints.ai_flags = AI_PASSIVE;
+    }
+
+
+    int getaddrinfo_res = getaddrinfo(address, port, &hints, results);
+    if (getaddrinfo_res != 0) {
+        const char* errmsg = gai_strerror(getaddrinfo_res);
+        printf("%s\n", errmsg);
+        return getaddrinfo_res;
+    }
+
+    int socket_fd = socket((*results)->ai_family, (*results)->ai_socktype, (*results)->ai_protocol);
+    if (socket_fd < 0) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+    }
+
+    return socket_fd;
+}
+
 int socket_test() {
     struct addrinfo hints;
     struct addrinfo *results;
@@ -105,20 +134,88 @@ int socket_test() {
         return getaddrinfoResult;
     }
     
-    int fd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-    if (fd == -1) {
+    int socketfd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+    if (socketfd == -1) {
         const char* error = strerror(errno);
         printf("%s\n", error);
-        return getaddrinfoResult;
+        return socketfd;
     }
 
-    int bindResult = bind(fd, results->ai_addr, results->ai_addrlen);
+    int bindResult = bind(socketfd, results->ai_addr, results->ai_addrlen);
     if (bindResult == -1) {
         const char* error = strerror(errno);
         printf("%s\n", error);
-        return getaddrinfoResult;
+        return bindResult;
     }
+
+    return 0;
+}
+
+int connect_test() {
+    struct addrinfo* addr;
     
+    int socket_fd = get_socket("www.google.com", &addr, "80");
+
+    int connectResult = connect(socket_fd, addr->ai_addr, addr->ai_addrlen);
+    if (connectResult == -1) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+        return connectResult;
+    }
+
+    return 0;
+}
+
+int listen_test() {
+    struct addrinfo* addr;
+    
+    int socket_fd = get_socket(NULL, &addr, "5000");
+
+    int bind_result = bind(socket_fd, addr->ai_addr, addr->ai_addrlen);
+    if (bind_result) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+        return bind_result;
+    }
+
+    int listenResult = listen(socket_fd, 10);
+    if (listenResult == -1) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+        return listenResult;
+    }
+
+    return 0;
+}
+
+int accept_test() {
+    struct addrinfo* addr;
+    
+    int socket_fd = get_socket(NULL, &addr, "5001");
+
+    int bind_result = bind(socket_fd, addr->ai_addr, addr->ai_addrlen);
+    if (bind_result == -1) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+        return bind_result;
+    }
+
+    int listenResult = listen(socket_fd, 10);
+    if (listenResult == -1) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+        return listenResult;
+    }
+
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size = sizeof their_addr;
+    int connected_fd = accept(socket_fd, (struct sockaddr*)&their_addr, &addr_size);
+    if (connected_fd) {
+        const char* error = strerror(errno);
+        printf("%s\n", error);
+        return connected_fd;
+    }
+
     return 0;
 }
 
@@ -127,4 +224,7 @@ int main() {
     machine_to_human();
     make_dns_lookup();
     socket_test();
+    connect_test();
+    listen_test();
+    accept_test();
 }
